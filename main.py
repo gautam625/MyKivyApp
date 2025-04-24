180750953b18af5fbf11ad34fdc5dd751bf3fc23
Gautam Kumar <103721382+gautam625@users.noreply.github.com>
2025-04-24 23:12:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,12 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
-from kivy.uix.label import Label
-
-
-class MyLayout(BoxLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.orientation = "vertical"
-
-        self.label = Label(text="Press the button")
-        self.add_widget(self.label)
-
-        self.button = Button(text="Click Me!")
-        self.button.bind(on_press=self.on_button_click)
-        self.add_widget(self.button)
-
-    def on_button_click(self, instance):
-        self.label.text = "Button Clicked!"
-
 
 class MyApp(App):
     def build(self):
-        return MyLayout()
+        return Button(text='Click Me!', on_press=self.say_hello)
 
+    def say_hello(self, instance):
+        print("Hello from Kivy!")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     MyApp().run()
